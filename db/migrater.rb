eb7c77fad47eb0e8d4276a0c897b0571a5e57fd5
mlaakso@dev <laakso.mavrick@gmail.com>
2018-12-01 01:47:51 -0500

    housekeeping

--- db/migrater.rb
+++ db/migrater.rb
@@ -1,3 +1,5 @@
+
+# eventually allow args for environment and opts
 def execute_script(file_name)
   (`psql "dbname='goals_development' user='postgres' password='postgres'" -h 127.0.0.1 -p 5432 -a -f db/migrations/#{file_name}`)
 end
@@ -10,6 +12,6 @@ file_names.each do |file_name|
   execute_script(file_name)
 end
 
-# find the last executed migration (if it exists)
+# here for if/when keeping track of the last ran migration matters
 #open("last_migration", "w") { |f| f << some_file_name }
 #open("last_migration", "r") { |f| p f.read() }
