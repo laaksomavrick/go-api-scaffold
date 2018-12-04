e349ae02edd33933d8ca605afcec1235a8409faa
Mavrick Laasko <laakso.mavrick@gmail.com>
2018-12-04 16:31:48 -0500

    wip

--- src/auth/handlers.go
+++ src/auth/handlers.go
@@ -4,9 +4,7 @@ import (
 	"encoding/json"
 	"io/ioutil"
 	"net/http"
-	"time"
 
-	jwt "github.com/dgrijalva/jwt-go"
 	"github.com/laaksomavrick/goals-api/src/core"
 	"github.com/laaksomavrick/goals-api/src/user"
 )
@@ -44,28 +42,16 @@ func Create(s *core.Server) http.HandlerFunc {
 			return
 		}
 
-		// sign and return jwt
-		token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
-			"userId": dbUser.ID,
-			"email":  dbUser.Email,
-			"nbf":    time.Date(2015, 10, 10, 12, 0, 0, 0, time.UTC).Unix(),
-		})
-
-		// TODO: store this token in db; invalidate old token if relogging in
-		// TODO: real hmac secret; read from file
-		// if keyData, e := ioutil.ReadFile("test/hmacTestKey"); e == nil {
-		// 	hmacSampleSecret = keyData
-		// } else {
-		// 	panic(e)
-		// }
-		tokenString, err := token.SignedString([]byte("asdqweasdqwe"))
+		// generate token
+		service := NewService()
+		token, err := service.GenerateToken(dbUser)
 		if err != nil {
 			core.EncodeJSONError(w, err, http.StatusInternalServerError)
 			return
 		}
 
 		json.NewEncoder(w).Encode(map[string]string{
-			"token": tokenString,
+			"token": token,
 		})
 
 	}
